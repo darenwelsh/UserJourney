7e4ee7e30a802ef9afebb8a8abd0dcc8b47f9720
Daren Welsh <darenwelsh@gmail.com>
2018-09-05 14:32:11 -0500

    use wfLoadExtension

--- UserJourney.php
+++ UserJourney.php
@@ -1,15 +1,53 @@
 <?php
+/**
+ * This extension tracks each user's journey as a wiki contributor
+ *
+ * Documentation: https://github.com/darenwelsh/UserJourney
+ * Support:       https://github.com/darenwelsh/UserJourney
+ * Source code:   https://github.com/darenwelsh/UserJourney
+ *
+ * @file UserJourney.php
+ * @addtogroup Extensions
+ * @author Daren Welsh
+ * @copyright © 2014 by Daren Welsh
+ * @licence GNU GPL v3+
+ */
 
-$wgExtensionCredits['specialpage'][] = array(
-	'path' => __FILE__,
-	'name' => 'UserJourney',
-	'author'=> array(
-		'[https://www.mediawiki.org/wiki/User:Darenwelsh Daren Welsh]'
-	),
-	'descriptionmsg' => 'userjourney-desc',
-	'version' => 0.1,
-	'url' => 'https://www.mediawiki.org/wiki/Extension:UserJourney',
-);
+# Not a valid entry point, skip unless MEDIAWIKI is defined
+if (!defined('MEDIAWIKI')) {
+	die( "UserJourney extension" );
+}
+
+// if wfLoadExtension exists (MW 1.25+) use that method of registering extensions
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'UserJourney' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['UserJourney'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['UserJourneyMagic'] = __DIR__ . '/Magic.php';
+	wfWarn(
+		'Deprecated PHP entry point used for UserJourney extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+
+// For MW 1.24 and lower, use normal extension registration
+// This should be removed when this extension drops support for <1.25
+} else {
+	
+	$wgExtensionCredits['specialpage'][] = array(
+		'path' => __FILE__,
+		'name' => 'UserJourney',
+		'author'=> array(
+			'[https://www.mediawiki.org/wiki/User:Darenwelsh Daren Welsh]'
+		),
+		'descriptionmsg' => 'userjourney-desc',
+		'version' => 0.1,
+		'url' => 'https://www.mediawiki.org/wiki/Extension:UserJourney',
+	);
+
+}
+
 
 /*
  * Configure global variables
